3dd080c60147440514f4665945b26b930bb54f43
Sara Webb <saracharlotterose@gmail.com>
2020-08-04 15:54:53 -0700

    Adds boiler plate code for Indexedb

---
+++ public/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+      );
+      
+    // pre cache all static assets
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+});
