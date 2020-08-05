d3d0cad83f7c3e72339c706cc6c3c8a2b4de413d
Sara Webb <saracharlotterose@gmail.com>
2020-08-05 16:20:28 -0700

    Adding Indexdb & service worker functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,43 @@
+
+
+const cache = "static-cache-v1"
+const precachefiles = [
+    "/",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "/icons/icon-192x192.png",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/index.js",
+    "/index.html",
+    "/db.js"
+
+
+]
+
 self.addEventListener("install", function (evt) {
     // pre cache image data
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-      );
+
+
+
+   
       
     // pre cache all static assets
     evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+      caches.open(cache).then((cache) => cache.addAll(precachefiles))
     );
 
     // tell the browser to activate this service worker immediately once it
     // has finished installing
     self.skipWaiting();
 });
+
+self.addEventListener("activate", ()=> {
+    self.clients.claim();
+})
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            return response || fetch(event.request);
+        })
+    )
+})
